1956a9235bcd503c69199b5cdc40c29e95073dbe
paulAlexSerban <paul.alex.serban@gmail.com>
2024-03-07 16:30:19 +0200

    fcc(be-api--express): typos

--- myApp.js
+++ myApp.js
@@ -5,13 +5,13 @@ const staticMiddleware = express.static(__dirname + '/public');
 
 app.use('/public', staticMiddleware);
 
+app.get('/json', (req, res) => {
+    res.json({ "message": "Hello json" });
+});
+
 app.get('/', (req, res) => {
     const indexFile = __dirname + '/views/index.html';
     res.sendFile(indexFile);
-});
-
-app.get('/json', (req, res) => {
-    res.json({ message: 'Hello json' });
 });
 
 module.exports = app;
