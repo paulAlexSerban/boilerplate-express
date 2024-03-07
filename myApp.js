aa80c07849437c48ee3562504a4ba4fa2268ec61
paulAlexSerban <paul.alex.serban@gmail.com>
2024-03-07 14:11:52 +0200

    fcc(be-api--express): serve static files

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
 
+const staticMiddleware = express.static(__dirname + '/public');
+
+app.use(staticMiddleware);
+
 app.get('/', (req, res) => {
     const indexFile = __dirname + '/views/index.html';
     res.sendFile(indexFile);
